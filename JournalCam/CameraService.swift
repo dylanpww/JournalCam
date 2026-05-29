8c6548f879e8ae49b179061d49108cdb4b9dcbad
RickykennardW <153189273+RickykennardW@users.noreply.github.com>
2026-05-29 10:03:21 +0700

    Initial Commit

---
+++ JournalCam/CameraService.swift
@@ -0,0 +1,18 @@
+//
+//  CameraService.swift
+//  JournalCam
+//
+//  Created by student on 29/05/26.
+//
+
+import SwiftUI
+
+struct CameraService: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CameraService()
+}
---
+++ JournalCam/JournalCamApp.swift
@@ -0,0 +1,18 @@
+//
+//  JournalCamApp.swift
+//  JournalCam
+//
+//  Created by student on 29/05/26.
+//
+
+import SwiftUI
+
+@main
+struct JournalCamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
+
